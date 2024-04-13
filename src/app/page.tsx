9aa84f40f916ab072bb0294f57bf0db0eda38ddf
Iván <ivanlitt8@gmail.com>
2024-04-12 22:40:16 -0300

    pre-branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
     <div className="min-h-screen bg-white overflow-hidden">
       <main className="relative z-20 ">
         <nav className="pt-5 z-20 mx-0 sm:ml-10 w-full sm:w-40 items-center sm:items-start">
-          <div className="-top-2 left-1/3 min-[636px]:left-0 absolute">
+          <div className="-top-2 left-1/4 min-[636px]:left-0 absolute">
             <Square width="150" height="140" fill="#FFE819" rx="20" />
           </div>
           <div className="-top-5 min-[636px]:left-24 right-1/2 absolute">
@@ -60,7 +60,7 @@ export default function Home() {
           </div>
           <Logo />
         </nav>
-        <div className="mx-5 sm:mx-8 md:mx-16 lg:mx-24 xl:mx-10 2xl:mx-60 ">
+        <div className="mx-5 sm:mx-8 md:mx-16 lg:mx-24 xl:mx-10 2xl:mx-60 pt-0 sm:pt-40">
           <div className="flex flex-col justify-normal sm:justify-center pt-10 sm:pt-0 h-1/2vh sm:h-screen w-full">
             <div className="-top-10 right-40 absolute hidden sm:block">
               <SquareTest width="200" height="200" fill="#8770FF" rx="50" />
@@ -74,14 +74,14 @@ export default function Home() {
             <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-9xl font-extrabold text-violet z-50 pt-10 sm:pt-0">
               ¡Descubre la revolución del Business Intelligence con Boty!
             </h1>
-            <p className="z-50 text-black mt-5 text-left text-sm sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl w-3/4">
+            <p className="z-50 text-black mt-5 text-left text-sm sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl min-[450px]:w-3/4 min-[642px]:w-full w-full">
               Maximiza tu <strong>rentabilidad</strong>,{" "}
               <strong>automatiza</strong> tus procesos y libera tu mente para
               enfocarte en lo que realmente importa:
               <strong> tu estrategia</strong>.
             </p>
             <br></br>
-            <div className="flex justify-center pt-0 sm:pt-8">
+            <div className="flex justify-center pt-0 md:pt-20 xl:pt-40">
               <div className="flex flex-col items-center">
                 <a
                   href="https://app.boty.cloud/login"
